70f26dbcd1db9a49d3f02798c2c5893458c1758a
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2023-11-26 14:04:56 -0500

    fix margin

--- src/pages/StartPage.tsx
+++ src/pages/StartPage.tsx
@@ -7,7 +7,7 @@ export const StartPage = () => {
     <>
       <section className="container pt-24 px-6 mx-auto max-w-lg">
         <Rune className="w-24 h-24 mx-auto" />
-        <h1 className="text-3xl text-center font-medium mt-16">
+        <h1 className="text-3xl text-center font-medium mt-8 leading-normal">
           Rune - Awesome UI for Your Next Local LLM Project
         </h1>
         <p className="text-center text-base-content/60 mt-4">Start from PWA</p>
